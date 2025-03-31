7bc0534fd2f5908448854ed06e6f7f18efa3ace6
Asma Hachelef <asma.hachelef1@gmail.com>
2025-03-31 11:49:03 +0200

    creation fichier client.java

---
+++ src/Client.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Client {
+    private int id;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private String codePostal;
+    private String ville;
+    private List<Commande> commandes;
+
+    public Client(int id, String nom, String prenom, String adresse, String codePostal, String ville){
+        this.id= id;
+        this.nom= nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.codePostal= codePostal;
+        this.ville = ville;
+        this.commandes = new ArrayList<>();
+    }
+    
+}

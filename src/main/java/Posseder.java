c47aaf31a352ae6a45ae25bed827bd9c7a374f18
Erwan <180167968+ErwanStudent@users.noreply.github.com>
2025-04-28 11:22:01 +0200

    fix: fix start

--- src/main/java/Posseder.java
+++ src/main/java/Posseder.java
@@ -1,10 +1,8 @@
 public class Posseder {
-    private Magasin magasin;
     private Livre livre;
     private int quantite;
 
     public Posseder(Magasin magasin, Livre livre, int quantite) {
-        this.magasin = magasin;
         this.livre = livre;
         this.quantite = quantite;
     }

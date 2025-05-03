be041879a8e12687e9562a50996322317eb125ca
Erwan <contact@erwan2.fr>
2025-05-03 19:02:06 +0200

    fix: fix keyboard mistake

--- src/main/java/Administrateur.java
+++ src/main/java/Administrateur.java
@@ -3,7 +3,7 @@ public class Administrateur extends Personnel {
     /**
      * Créer un administrateur.
      * @param id L'identifiant de l'administrateur.
-     * @param nom Le nom du prénom de l'administrateur.s
+     * @param nom Le nom du prénom de l'administrateur.
      * @param prenom Le prénom de l'administrateur.
      */
     public Administrateur(int id, String nom, String prenom) {

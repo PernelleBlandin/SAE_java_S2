00229c2425532b44a2cc62c387f7d6aa8ea2ba22
AsmaH <asma.hachelef1@gmail.comgit config --listgit config --global user.email asma.hachelef1@gmail.com>
2025-05-04 22:51:53 +0200

    Début class Vendeur

--- src/main/java/Vendeur.java
+++ src/main/java/Vendeur.java
@@ -1,5 +1,10 @@
 /** Un vendeur */
 public class Vendeur extends Personnel {
+    private Magasin magasin;
+    private List<Commande> commandes;
+    private Panier panier;
+    private Magasin magasin; //le vendeur est associé à un magasin
+
     /**
      * Créer un vendeur.
      * @param id L'identifiant du vendeur.
@@ -11,5 +16,18 @@ public class Vendeur extends Personnel {
      */
     public Vendeur(int id, String nom, String prenom, String adresse, String codePostal, String ville) {
         super(id, nom, prenom, adresse, codePostal, ville, "Vendeur");
+    }
+
+    public void passerCommande(Client client, List<DetailCommande> detailCommande, char modeLivraison){
+        Commande commande= new Commande (1, Date.valueOf(LocalDate.now()), 'N', modeLivraison, this.magasin, List<DetailCommande> detailCommande )
+
+    }
+
+    public void transfererLivre(){
+
+    }
+
+    public void gererStock(){
+        
     }
 }

2251c1bb98edfafe8c93d78d61ea33a0cc1c6373
PernelleBlandin <pernelleblandin@gmail.com>
2025-06-17 09:44:39 +0200

    import admini home

--- src/main/java/vue/admin/AdminHomeView.java
+++ src/main/java/vue/admin/AdminHomeView.java
@@ -1,5 +1,35 @@
 package vue.admin;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import controleurs.ControleurDeconnexion;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import modeles.ChaineLibrairie;
+import modeles.Client;
+import modeles.Livre;
+import modeles.Magasin;
+import vue.AppIHM;
+import vue.BibliothequeComposants;
+import vue.MAJVueInterface;
+
+/** La vue de l'accueil d'un admin */
 public class AdminHomeView {
     
 }

f153eb65dc6f3816ba44d358d0c353825ab045ad
PernelleBlandin <pernelleblandin@gmail.com>
2025-06-18 18:05:14 +0200

    resolution des conflits et suppresion des imports en trop apres le merge

--- src/main/java/vue/admin/AdminView.java
+++ src/main/java/vue/admin/AdminView.java
@@ -1,44 +1,27 @@
 package vue.admin;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import controleurs.ControleurDeconnexion;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 
 import javafx.scene.Scene;
-import javafx.scene.chart.*;
-import javafx.scene.layout.Priority;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.TilePane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
 import modeles.ChaineLibrairie;
-import modeles.Client;
-import modeles.Vendeur;
-import modeles.Livre;
 import modeles.Magasin;
 
 import vue.AppIHM;
-import vue.BibliothequeComposants;
-import vue.MAJVueInterface;
 
 /** La vue de l'accueil d'un admin */
 public class AdminView {

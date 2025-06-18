b7d8386b03c663d98949b80a3d99f21e9c0f2d9c
PernelleBlandin <pernelleblandin@gmail.com>
2025-06-18 11:52:01 +0200

    nom boutons

--- src/main/java/vue/admin/AdminView.java
+++ src/main/java/vue/admin/AdminView.java
@@ -289,7 +289,8 @@ public Scene getScene() {
         lesMag.getChildren().addAll(titre);
 
         
-        try { for(Magasin unMag : this.modele.getMagasinBD().obtenirListeMagasin()) {
+        try { 
+            for(Magasin unMag : this.modele.getMagasinBD().obtenirListeMagasin()) {
             Image imgVendeurs = new Image("/images/multiple_sellers_silhouette.png");
             ImageView viewVendeurs = new ImageView(imgVendeurs);
             Image imgPoubelle = new Image("/images/trashcan.png");
@@ -305,13 +306,13 @@ public Scene getScene() {
 
             Button btnVendeursDuMag = new Button();
             btnVendeursDuMag.setGraphic(viewVendeurs);
-            Button btnsupprimerMag = new Button();
-            btnsupprimerMag.setGraphic(viewPoubelle);
+            Button btnSupprimerMag = new Button();
+            btnSupprimerMag.setGraphic(viewPoubelle);
 
-            lesBtn.getChildren().addAll(btnVendeursDuMag, btnsupprimerMag);
+            lesBtn.getChildren().addAll(btnVendeursDuMag, btnSupprimerMag);
 
             //btnVendeursDuMag.setOnAction(new ControleurVoirVendeurs(mag));
-            //btnsupprimerMag.setOnAction(new ControleurSupprMag(mag));
+            //btnSupprimerMag.setOnAction(new ControleurSupprMag(mag));
         
             ligneMag.setLeft(leMag);
             ligneMag.setRight(lesBtn);

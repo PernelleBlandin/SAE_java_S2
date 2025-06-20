8374ea34011272d7df2c5b32940594d0722a8bc8
AsmaH <asma.hachelef1@gmail.comgit config --listgit config --global user.email asma.hachelef1@gmail.com>
2025-06-20 08:53:32 +0200

    autre requete rené goscinny

--- src/main/java/modeles/StatistiquesBD.java
+++ src/main/java/modeles/StatistiquesBD.java
@@ -180,10 +180,14 @@ public class StatistiquesBD {
     public Map<String, Integer> getQteLivresGoscinyOrigineClients() throws SQLException {
         Statement statement = this.connexionMariaDB.createStatement();
         ResultSet resultSet = statement.executeQuery("""
-            SELECT villecli ville, SUM(qte) qte
-            FROM CLIENT NATURAL JOIN COMMANDE NATURAL JOIN DETAILCOMMANDE NATURAL JOIN LIVRE NATURAL JOIN ECRIRE NATURAL JOIN AUTEUR
-            WHERE nomauteur = 'René Goscinny'
-            GROUP BY villecli;
+            select distinct idcli, nomcli, prenomcli, adressecli, codepostal, villecli from CLIENT
+            natural join COMMANDE
+            natural join DETAILCOMMANDE
+            natural join LIVRE
+            natural join ECRIRE
+            natural join AUTEUR
+            where YEAR(datecom)=2021 and nomauteur= 'René Goscinny'
+            order by idcli;
         """);
 
         Map<String, Integer> statistiques = new HashMap<>();
